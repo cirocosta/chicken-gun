7d4a5a166e73c9a0320aa86cb0d167c0cc23f0b4
Ciro S. Costa <cscosta@pivotal.io>
2019-03-15 19:02:38 -0400

    start adding cli
    Signed-off-by: Ciro S. Costa <cscosta@pivotal.io>

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,46 @@
-use std::io;
+#[macro_use]
+
+extern crate clap;
+extern crate num_cpus;
+
+use clap::{App, Arg, ArgMatches, SubCommand};
+use std::process;
+
+fn run(matches: ArgMatches) -> Result<(), String> {
+    match matches.subcommand() {
+        ("user-cpu", Some(m)) => run_user_cpu(m),
+        ("system-cpu", Some(m)) => run_system_cpu(m),
+        _ => Ok(()),
+    }
+}
+
+fn run_user_cpu(matches: &ArgMatches) -> Result<(), String> {
+    let threads = value_t!(matches, "threads", usize).unwrap_or(4);
+
+    println!("num of threads = {}", threads);
+
+    Ok(())
+}
+
+fn run_system_cpu(_matches: &ArgMatches) -> Result<(), String> {
+    Ok(())
+}
 
 fn main() {
-    println!("Hello, world!");
-    
-    // `::` denotes that a particular method is an
-    // ASSOCIATED FUNCTION that is implemented on the
-    // TYPE, not the instance of that type itself.
-    let mut guess = String::new();
-    
-    io::stdin()
-        .read_line(&mut guess)          // `&` indicates that we're passing a reference, but
-        .expect("failed to read line"); // like values, references are immutable by default.
-                                        // TODO why does this need to be mutable though?
+    let matches = App::new("chicken-gun")
+        .version(env!("CARGO_PKG_VERSION"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .subcommand(SubCommand::with_name("user-cpu")
+                .about("drive user cpu utilization to the top"))
+                .arg(Arg::with_name("threads")
+                    .short("t")
+                    .help("number of threads to use"))
+        .subcommand(SubCommand::with_name("system-cpu")
+                .about("drive system cpu utilization to the top"))
+        .get_matches();
 
-    println!("you guessed: {}", guess);
+    if let Err(e) = run(matches) {
+        println!("error: {}", e);
+        process::exit(1);
+    }
 }
