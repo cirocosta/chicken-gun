78bfc4db8d9e0b00d2a2b531e3fc8250e96a71d6
Ciro S. Costa <cscosta@pivotal.io>
2019-03-15 20:01:08 -0400

    fmt + actual consumption of userspace time in user-cpu
    Signed-off-by: Ciro S. Costa <cscosta@pivotal.io>

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate clap;
 extern crate num_cpus;
 
 use clap::{App, Arg, ArgMatches, SubCommand};
-use std::process;
+use std::{process, thread};
 
 fn run(matches: ArgMatches) -> Result<(), String> {
     match matches.subcommand() {
@@ -15,9 +15,16 @@ fn run(matches: ArgMatches) -> Result<(), String> {
 }
 
 fn run_user_cpu(matches: &ArgMatches) -> Result<(), String> {
-    let threads = value_t!(matches, "threads", usize).unwrap_or(4);
+    let thread_num = value_t!(matches, "threads", usize).unwrap();
+    let mut child_threads = vec![];
 
-    println!("num of threads = {}", threads);
+    for _ in 0..thread_num {
+        child_threads.push(thread::spawn(|| loop {}))
+    }
+
+    for child in child_threads {
+        let _ = child.join();
+    }
 
     Ok(())
 }
@@ -30,13 +37,19 @@ fn main() {
     let matches = App::new("chicken-gun")
         .version(env!("CARGO_PKG_VERSION"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
-        .subcommand(SubCommand::with_name("user-cpu")
-                .about("drive user cpu utilization to the top"))
-                .arg(Arg::with_name("threads")
-                    .short("t")
-                    .help("number of threads to use"))
-        .subcommand(SubCommand::with_name("system-cpu")
-                .about("drive system cpu utilization to the top"))
+        .subcommand(
+            SubCommand::with_name("user-cpu")
+                .about("drive user cpu utilization to the top")
+                .arg(
+                    Arg::with_name("threads")
+                        .default_value("4")
+                        .short("t")
+                        .help("number of threads to use"),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("system-cpu").about("drive system cpu utilization to the top"),
+        )
         .get_matches();
 
     if let Err(e) = run(matches) {
