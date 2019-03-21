85d0d0fe7a7008eb2a676bf9de77dcb4b13b9599
Ciro S. Costa <cscosta@pivotal.io>
2019-03-21 17:16:47 +0000

    keep files open until execution finishes
    Signed-off-by: Ciro S. Costa <cscosta@pivotal.io>

--- src/fs.rs
+++ src/fs.rs
@@ -2,16 +2,24 @@
 
 use std::{fs, path, thread, time};
 
+/// Creates and keep open a bunch of files.
+///
+/// - files get created under `directory` and *not* cleaned afterward.
+/// - files remain open until the end of the execution (once the process
+///   gets signalled).
+///
 pub fn exercise_files_open(num: usize, directory: &str) {
+    let mut open_files = Vec::with_capacity(num);
+
     for idx in 0..num {
         let filepath = path::Path::new(directory).join(idx.to_string());
 
-        match fs::File::create(&filepath) {
+        let file = match fs::File::create(&filepath) {
             Err(why) => panic!("failed to create {}: {}", filepath.display(), why),
             Ok(file) => file,
         };
 
-        // it closes the file for some reason :thinking:
+        open_files.push(file);
     }
 
     thread::sleep(time::Duration::from_secs(1 << 16))
